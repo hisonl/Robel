da7e9561d00b72244927dd1b7f41ad0d78a78d0b
Heesung Lee <bespoke.il.micio@gmail.com>
2012-08-21 10:03:36 +0900

    added ch01

---
+++ ch01/Hello.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namesoace std;
+
+int main() {
+    cout << "2 + 2 = " << 2 + 2 << endl;
+    cout << "2 - 2 = " << 2 - 2 << endl;
+    cout << "2 * 2 = " << 2 * 2 << endl;
+    cout << "2 / 2 = " << 2 / 2 <<
